4c4ff4302192e79d415098e81095bd3cc5bea503
denis <permwed@gmail.com>
2023-01-19 17:14:35 +0500

    create first hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,12 @@
-import { useEffect, useState } from "react";
-import axios, { AxiosError } from "axios";
 // Components
 import Product from "./components/Product/Product";
-// Model
-import { IProduct } from "./models";
+// hooks
+import { useProducts } from "./hooks/products";
 // Css
 import "./App.css";
 
 const App = () => {
-  const [products, setProducts] = useState<IProduct[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
-
-  async function fetchProducts() {
-    try {
-      setError("");
-      setLoading(true);
-      const response = await axios.get<IProduct[]>(
-        "https://fakestoreapi.com/products"
-      );
-      setProducts(response.data);
-      setLoading(false);
-    } catch (e: unknown) {
-      const error = e as AxiosError;
-      setLoading(false);
-      setError(error.message);
-    }
-  }
-
-  useEffect(() => {
-    fetchProducts();
-  }, []);
+  const { products, error, loading } = useProducts();
 
   return (
     <div className="App">
